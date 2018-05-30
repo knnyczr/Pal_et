7a63103b731483855e0e72668a130c214b3e01ca
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2018-05-30 19:47:22 -0400

    okay

--- backend-express/routes/palettes.js
+++ backend-express/routes/palettes.js
@@ -3,15 +3,29 @@ var router = express.Router();
 
 palModels = require('../models/palette.js')
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  palModels.showAll()
-    .then(pals => {
-      res.json({pals:pals});
-    }).catch(err =>{
+/* get ONE pal palette by it's id */
+router.get('/:id', (req, res, next) => {
+  palModels.getOne(req.params.id)
+    .then(pal => {
+      res.json({pal:pal});
+    })
+    .catch(err => {
       console.log(err)
       res.status(500).json({err:err})
     })
 });
+
+/* GET all the pal palettes */
+router.get('/', (req, res, next) => {
+  palModels.showAll()
+    .then(pals => {
+      res.json({pals:pals});
+    })
+    .catch(err =>{
+      console.log(err)
+      res.status(500).json({err:err})
+    })
+});
+
 
 module.exports = router;
