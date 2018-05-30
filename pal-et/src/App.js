c9586cea382f5de4fb229805307cd4a3a903082b
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2018-05-30 17:04:09 -0400

    whoops havent commited anything

--- pal-et/src/App.js
+++ pal-et/src/App.js
@@ -3,23 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = { users:[] }
-
-  componentDidMount(){
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }))
+  state = {
+    users:[],
+    palette:[]
   }
 
+  componentDidMount(){
+    // e.preventDefault();
+    let url = "http://colormind.io/api/";
+    let data = {
+      model: "default",
+      input: ["N","N","N","N","N"]
+    }
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+      if (http.readyState === 4 && http.status === 200) {
+        this.setState({ palette: (JSON.parse(http.responseText).result) })
+      }
+    }
+    http.open("POST", url, true);
+    http.send(JSON.stringify(data));
+  }
+
+  getpal(e){
+    e.preventDefault();
+    let url = "http://colormind.io/api/";
+    let data = {
+      model: "default",
+      input: ["N","N","N","N","N"]
+    }
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = () => {
+      if (http.readyState === 4 && http.status === 200) {
+        this.setState({ palette: (JSON.parse(http.responseText).result) })
+      }
+    }
+    http.open("POST", url, true);
+    http.send(JSON.stringify(data));
+  }
+
+
   render() {
+    console.log(this.state.palette)
     return (
       <div className="App">
-        <h1>users</h1>
+        {/* <h1>users</h1>
           <ul>
-            {this.state.users.map(users =>
-              <li key={users.id}>{users.username}</li>
-            )}
-          </ul>
+            {this.state.users.map(users => {
+              return <li key={users.id}>{users.username}</li>
+            })}
+          </ul> */}
+          <button onClick={(e)=> this.getpal(e)}>colors!</button>
+          <br />
+
+          <div className="pal box is-flex">
+            {
+              this.state.palette.map((d,i) => {
+                return <div
+                  className="palette"
+                  key={i}
+                  style={{backgroundColor: `rgb(${d})`}}
+                  >
+                  </div>
+              })
+            }
+          </div>
       </div>
     );
   }
