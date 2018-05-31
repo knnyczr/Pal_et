68ae95a40d8ff5e0a6b58877bfe88af513d18b98
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2018-05-31 18:42:43 -0400

    new functioning api

--- pal-et/src/App.js
+++ pal-et/src/App.js
@@ -3,56 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = {
-    users:[],
-    palette:[]
+  constructor(props){
+    super(props);
+    this.state = {
+      users:[],
+      palette:[]
+    }
+  }
+
+  getexpress(){
+    fetch(`/api`)
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 
   componentDidMount(){
-    let url = "http://colormind.io/api/";
-    let data = {
-      model: "default",
-      input: ["N","N","N","N","N"]
-    }
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = () => {
-      if (http.readyState === 4 && http.status === 200) {
-        this.setState({ palette: (JSON.parse(http.responseText).result) })
-      }
-    }
-    http.open("POST", url, true);
-    http.send(JSON.stringify(data));
+    fetch(`http://www.colr.org/json/colors/random/5`)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data.colors)
+      let colors = data.matching_colors
+      this.setState({palette : colors})
+    })
+
+
   }
 
-  getpal(e){
-    e.preventDefault();
-    let url = "http://colormind.io/api/";
-    let data = {
-      model: "default",
-      input: ["N","N","N","N","N"]
-    }
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = () => {
-      if (http.readyState === 4 && http.status === 200) {
-        this.setState({ palette: (JSON.parse(http.responseText).result) })
-      }
-    }
-    http.open("POST", url, true);
-    http.send(JSON.stringify(data));
-  }
+
 
 
   render() {
     console.log(this.state.palette)
     return (
       <div className="App">
-        {/* <h1>users</h1>
-          <ul>
-            {this.state.users.map(users => {
-              return <li key={users.id}>{users.username}</li>
-            })}
-          </ul> */}
-          <button onClick={(e)=> this.getpal(e)}>colors!</button>
+          <button onClick={(e) => {this.getexpress(e)}} >get express</button>
+          {/* <button onClick={(e)=> this.getpal(e)}>colors!</button> */}
           <br />
 
           <div className="pal box is-flex">
@@ -61,8 +46,9 @@ class App extends Component {
                 return <div
                   className="palette"
                   key={i}
-                  style={{backgroundColor: `rgb(${d})`}}
+                  style={{backgroundColor: `#${d}`}}
                   >
+                    {this.state.palette[i]}
                   </div>
               })
             }
--- pal-et/src/index.js
+++ pal-et/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bulma/css/bulma.css';
-import { Router, Route, Switch } from 'react-router'
+import { Router, Route, Switch } from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
